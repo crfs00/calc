03d380f766def9f741870e078c6fdce21ae892dd
Curtis Stovell <crfs@protonmail.com>
2019-05-07 22:00:35 +0100

    Initial commit

---
+++ Calc_Main.py
@@ -0,0 +1,17 @@
+from Calc import Calculator as Cal
+import os
+
+
+def main():
+
+    print(Cal.add(1, 3))
+    print(Cal.subtract(20, 3))
+    print(Cal.multiply(3, 3))
+    print(Cal.divide(9, 3))
+
+
+if __name__ == '__main__':
+    main()
+
+
+os.rename('thing.txt', '.gitignore')
