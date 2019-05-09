9906696b1273c8d7ca11bdf567a8958f9f6b59f6
Curtis Stovell <crfs@protonmail.com>
2019-05-09 21:32:47 +0100

    Added new fucntions and a README

--- Calc.py
+++ Calc.py
@@ -1,12 +1,37 @@
 class Calculator:
+
     def add(x, y):
+        '''
+        Function for handling Addition
+        '''
         return x + y
 
     def subtract(x, y):
+        '''
+        Function for handling Subtraction
+        '''
         return x - y
 
     def multiply(x, y):
+        '''
+        Function for handling Mutiplication
+        '''
         return x * y
 
     def divide(x, y):
+        '''
+        Function for handling Division
+        '''
         return x * y
+
+    def exponent(x, y):
+        '''
+        Function for handling Exponents
+        '''
+        return x**y
+
+    def floor_div(x, y):
+        '''
+        Function for handling Floor Division
+        '''
+        return x // y
--- Calc_Main.py
+++ Calc_Main.py
@@ -7,6 +7,8 @@ def main():
     print(Cal.subtract(20, 3))
     print(Cal.multiply(3, 3))
     print(Cal.divide(9, 3))
+    print(Cal.exponent(5, 3))
+    print(Cal.floor_div(26, 3))
 
 
 if __name__ == '__main__':
