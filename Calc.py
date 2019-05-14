64321ebcb85cbc61a71365e9767047133449260f
Curtis Stovell <crfs@protonmail.com>
2019-05-14 19:41:55 +0100

    Minor upkeep. Unit testing next

--- Calc.py
+++ Calc.py
@@ -1,23 +1,30 @@
 class Calculator:
 
-    def validate(x):
+    def validate(arg_list_to_validate):
+        '''
+        Ensure that all elements are digits and convert strings to ints
+
+        :param arg_list_to_validate: A list of digits in int, float, or string form
+        :rtype A list of floats if no errors, 'None' otherwise.
+        '''
         try:
-            x = list(map(int, x))
+            arg_list_to_validate = list(map(float, arg_list_to_validate))
         except:
             print('Please only enter numbers.')
             return None
-        return x
+        return arg_list_to_validate
 
-    def failed_validation_msg(x):
-        print(f'This is what you entered: {x}')
+    def failed_validation_msg(invalid_list):
+        '''Output the values that the user entered if the validate method finds an error'''
+        print(f'This is what you entered: {invalid_list}')
 
-    def add(*x):
+    def add(*args_list_to_add):
         '''Function for handling Addition'''
-        y = Calculator.validate(x)
-        if y:
-            print(sum(y))
+        validate_list = Calculator.validate(args_list_to_add)
+        if validate_list:
+            print(sum(validate_list))
         else:
-            Calculator.failed_validation_msg(x)
+            Calculator.failed_validation_msg(args_list_to_add)
 
     def subtract(*x):
         '''Function for handling Subtraction'''
--- Calc_Main.py
+++ Calc_Main.py
@@ -3,7 +3,7 @@ from Calc import Calculator as Cal
 
 def main():
 
-    Cal.add(95, 95, 95, 95, 95, 95, 96, '4')
+    Cal.add(95, 95, 95, 95, 95, 95, 96, '4.5')
     # Cal.subtract(20, 1, 1, -1, 'f')
     # print(Cal.multiply(3, 3, 2, 5))
     # print(Cal.divide(6606, 7, 5, 3, 2,))
